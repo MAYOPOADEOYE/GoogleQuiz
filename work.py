9f722343e209ad0b90a8a28e47052231605539cd
Mayopo <adeoyemayopo@gmail.com>
2021-07-14 14:32:55 +0100

    Commiting work.py file

---
+++ work.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root=Tk()
+root.title('Gooogle playstore')
+root.configure(bg="blue")
+
+make=Entry(root,width=35,borderwidth=4)
+make.grid(row=0,column=0,columnspan=3,padx=15,pady=15)
